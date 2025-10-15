be1949a2ec5bc4517d3ce66ef6864cf809fc3b4d
usman ahmed <usikandar85@gmail.com>
2025-10-15 23:42:28 +0500

    first commit after removing dependencies errors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,19 +7,21 @@ import { Ionicons } from '@expo/vector-icons';
 import { ActivityIndicator, TouchableOpacity, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+console.log(process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,"publish key");
+
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 // Cache the Clerk JWT
 const tokenCache = {
-  async getToken(key: string) {
+  async getToken (key: string) {
     try {
-      return SecureStore.getItemAsync(key);
+      return await SecureStore.getItemAsync(key);
     } catch (err) {
       return null;
     }
   },
   async saveToken(key: string, value: string) {
     try {
-      return SecureStore.setItemAsync(key, value);
+      return await SecureStore.setItemAsync(key, value);
     } catch (err) {
       return;
     }
